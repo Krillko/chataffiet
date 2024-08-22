11fed93774e65f7818b64a6e6287f9f0ea0d95e1
Kristoffer Ekendahl <kristoffer.ekendahl@iveo.se>
2024-08-22 23:10:59 +0200

    Why is stackblitz fighting with me

--- src/components/AffieChat.vue
+++ src/components/AffieChat.vue
@@ -1,70 +1,73 @@
 <template>
-  <div class="w-1/2">
-    <div v-for="(msg, index) in chat" :key="index">
-      <div class="chat"
-      :class="{
-        'chat-start': msg.part === 'user',
-        'chat-end': msg.part === 'affie',
-      }">
-        <div class="chat-image avatar">
-          <div class="w-24 rounded-full">
-            <img
-              v-if="msg.part === 'user'"
-              src="/icons/user.jpg"
-            >
-            <img v-else :src="msg.img">
+  <div>
+    <div ><img src="/headerimg.jpg" class="max-h-[300px]"></div>
+    <div class="text-4xl">Välkommen till <span class="font-bold">ChatAffieT</span></div>
+    <div class="w-1/2">
+      <div v-for="(msg, index) in chat" :key="index">
+        <div class="chat"
+             :class="{
+          'chat-start': msg.part === 'user',
+          'chat-end': msg.part === 'affie',
+        }">
+          <div class="chat-image avatar">
+            <div class="w-24 rounded-full">
+              <img
+                v-if="msg.part === 'user'"
+                src="/icons/user.jpg"
+              >
+              <img v-else :src="msg.img">
+            </div>
           </div>
-        </div>
-        <div class="chat-header text-xs">
-          <span v-if="msg.part === 'user'">Du:</span>
-          <span v-if="msg.part === 'affie'">Affie:</span>
-        </div>
-        <div class="chat-bubble">{{ msg.message }}</div>
-      </div>
-    </div>
-<!--    <div class="chat chat-start">
-      <div class="chat-image avatar">
-        <div class="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS chat bubble component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          <div class="chat-header text-xs">
+            <span v-if="msg.part === 'user'">Du:</span>
+            <span v-if="msg.part === 'affie'">Affie:</span>
+          </div>
+          <div class="chat-bubble">{{ msg.message }}</div>
         </div>
       </div>
-      <div class="chat-header">
-        Obi-Wan Kenobi
-        <time class="text-xs opacity-50">12:45</time>
-      </div>
-      <div class="chat-bubble">You were the Chosen One!</div>
-      <div class="chat-footer opacity-50">Delivered</div>
-    </div>-->
-<!--
-    <div class="chat chat-end">
-      <div class="chat-image avatar">
-        <div class="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS chat bubble component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
-      </div>
-      <div class="chat-header">
-        Anakin
-        <time class="text-xs opacity-50">12:46</time>
-      </div>
-      <div class="chat-bubble">I hate you!</div>
-      <div class="chat-footer opacity-50">Seen at 12:46</div>
-    </div>
--->
+      <!--    <div class="chat chat-start">
+            <div class="chat-image avatar">
+              <div class="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS chat bubble component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+            <div class="chat-header">
+              Obi-Wan Kenobi
+              <time class="text-xs opacity-50">12:45</time>
+            </div>
+            <div class="chat-bubble">You were the Chosen One!</div>
+            <div class="chat-footer opacity-50">Delivered</div>
+          </div>-->
+      <!--
+          <div class="chat chat-end">
+            <div class="chat-image avatar">
+              <div class="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS chat bubble component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+            <div class="chat-header">
+              Anakin
+              <time class="text-xs opacity-50">12:46</time>
+            </div>
+            <div class="chat-bubble">I hate you!</div>
+            <div class="chat-footer opacity-50">Seen at 12:46</div>
+          </div>
+      -->
 
-
-    <label class="form-control w-full max-w-xs">
-      <div class="label">
-        <span class="label-text">Skriv till Affie</span>
-      </div>
-      <div class="flex"><input type="text" placeholder="Här alltså" class="input input-bordered w-full max-w-xs"
-                                v-model="input" @keydown.enter="submitChat"/>
-        <button class="btn btn-primary" @click="submitChat">&gt;</button>
-      </div>
-    </label>
+      <label class="form-control w-full max-w-xs">
+        <div class="label">
+          <span class="label-text">Skriv till Affie</span>
+        </div>
+        <div class="flex"><input type="text" placeholder="Här alltså" class="input input-bordered w-full max-w-xs"
+                                 v-model="input" @keydown.enter="submitChat"/>
+          <button class="btn btn-primary" @click="submitChat">&gt;</button>
+        </div>
+      </label>
+    </div>
   </div>
 </template>
 
@@ -104,8 +107,6 @@ const submitChat = () => {
     })
 
     setTimeout(()=>{
-      console.log(chat.value.length)
-      console.log(chat.value)
       const index = chat.value.length - 1;
       chat.value[index] = {
         ...chat.value[index],
