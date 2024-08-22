18377a1aa4219ce709c2b9fe0bcd1628a5242908
Kristoffer Ekendahl <kristoffer.ekendahl@iveo.se>
2024-08-22 21:43:50 +0200

    Trying daisyui

--- src/components/AffieChat.vue
+++ src/components/AffieChat.vue
@@ -1,40 +1,35 @@
 <template>
   <div>
-    <div class="p-4">
-      <input type="checkbox" class="toggle" />
-      <input type="checkbox" class="toggle toggle-primary" />
-      <input type="checkbox" class="toggle toggle-secondary" />
-      <input type="checkbox" class="toggle toggle-accent" />
-      <br/>
-      <input type="checkbox" class="checkbox" />
-      <input type="checkbox" class="checkbox-primary checkbox" />
-      <input type="checkbox" class="checkbox-secondary checkbox" />
-      <input type="checkbox" class="checkbox-accent checkbox" />
-      <br/>
-      <input type="radio" name="radio" class="radio" />
-      <input type="radio" name="radio" class="radio-primary radio" />
-      <input type="radio" name="radio" class="radio-secondary radio" />
-      <input type="radio" name="radio" class="radio-accent radio" />
-    </div>
-    <details class="dropdown">
-      <summary class="btn m-1">open or close</summary>
-      <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li><a>Item 2</a></li>
-      </ul>
-    </details>
-
     <div class="chat chat-start">
-      <div class="chat-bubble">
-        It's over Anakin,
-        <br />
-        I have the high ground.
+      <div class="chat-image avatar">
+        <div class="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS chat bubble component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
       </div>
+      <div class="chat-header">
+        Obi-Wan Kenobi
+        <time class="text-xs opacity-50">12:45</time>
+      </div>
+      <div class="chat-bubble">You were the Chosen One!</div>
+      <div class="chat-footer opacity-50">Delivered</div>
     </div>
     <div class="chat chat-end">
-      <div class="chat-bubble">You underestimate my power!</div>
+      <div class="chat-image avatar">
+        <div class="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS chat bubble component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+      <div class="chat-header">
+        Anakin
+        <time class="text-xs opacity-50">12:46</time>
+      </div>
+      <div class="chat-bubble">I hate you!</div>
+      <div class="chat-footer opacity-50">Seen at 12:46</div>
     </div>
-
   </div>
 </template>
 
